224a29886ea0579f19841442195f06b312743d5e
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-12 17:57:03 +0200

    Empezado esqueleto del back

---
+++ Backend/src/main/java/EjemploArte/example/EjemploArte/Obras.java
@@ -0,0 +1,13 @@
+package EjemploArte.example.EjemploArte;
+
+public class Obras
+{
+    private int id;
+    private String nombre;
+    private double valor;
+    private int año;
+    private String categoria;
+    private String popularidad;
+
+
+}
