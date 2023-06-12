42127e452fb8170a1641bc2485115fa6090bc4e1
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-12 19:14:48 +0200

    Finalizado test de comprobar un objeto creado

--- Backend/src/test/java/EjemploArte/example/EjemploArte/DataHandingTest.java
+++ Backend/src/test/java/EjemploArte/example/EjemploArte/DataHandingTest.java
@@ -14,7 +14,9 @@ public class DataHandingTest {
         DataHanding data =  new DataHanding();
         Obras pr1 = new Obras(0,"Nuevo cuadro",129.36,85,"cuadro","Media");
         LeerJson reader = new LeerJson();
-        ArrayList<Obras> listaAux = reader.LeerFicheroJson1("Arte.json");
+        ArrayList<Obras> listaAux = new ArrayList<Obras>();
+        listaAux = data.AnadirObjeto1(pr1,"Arte.json");
+
         int comprobacion;
 
         comprobacion = data.ComprobarObjeto(pr1,listaAux);
@@ -39,10 +41,9 @@ public class DataHandingTest {
     {
         DataHanding data =  new DataHanding();
         Obras pr1 = new Obras(0,"Nuevo cuadro",129.36,85,"cuadro","Media");
-        Criptomoneda pr1 = new Criptomoneda(0,"SuperCoin",129,78.36,"Terciaria","Media");
         assertEquals(pr1.getNombre(),"SuperCoin");
         assertEquals(pr1.getValor(),129, 0.001);
-        assertEquals(pr1.getFluctuacion(),78.36, 0.001);
+        assertEquals(pr1.getEdad(),78.36, 0.001);
         assertEquals(pr1.getCategoria(),"Terciaria");
         assertEquals(pr1.getPopularidad(),"Media");
     }
